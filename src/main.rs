e98ce1f85f007f826ee67af6ff45f7b0052a1c3a
William Muchui <williammuchui584@gmail.com>
2024-10-05 10:37:50 +0300

    backbone of project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = args().collect::<Vec<String>>();
+    let args_len = arguments.len();
+
+    if args_len == 1 {
+        handle_list_argument();
+        return;
+    }
+
+    if args_len == 2 && arguments[1] == "-l" {
+        handle_list_argument();
+        return;
+    }
+
+    if args_len == 3 && arguments[1] == "-s" {
+        handle_search_argument(&arguments[2]);
+        return;
+    }
+
+    print_help();
+}
+
+fn handle_list_argument() {
+    //read file and display contents
+    return;
+}
+
+fn handle_search_argument(needle: &str) {
+    println!("{needle}");
+    return;
+}
+
+fn print_help() {
+    return;
 }
