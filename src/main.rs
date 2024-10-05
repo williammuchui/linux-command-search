7edc0625c3636ca82260eaf51c232bc4a4912a84
William Muchui <williammuchui584@gmail.com>
2024-10-05 12:06:54 +0300

    some little refactoring && pattern matching

--- src/main.rs
+++ src/main.rs
@@ -2,91 +2,59 @@ use std::env::args;
 use std::fs::File;
 use std::io::Read;
 use std::process::exit;
+
 const LINUX_FILE_PATH: &str = "linux";
 
 fn main() {
-    let arguments = args().collect::<Vec<String>>();
-    let args_len = arguments.len();
-
-    if args_len == 1 {
-        handle_list_argument();
-        return;
+    let arguments: Vec<String> = args().collect();
+    match arguments.as_slice() {
+        [_] => handle_list_argument(),
+        [_, arg] if arg == "-l" => handle_list_argument(),
+        [_, arg, needle] if arg == "-s" => handle_search_argument(needle),
+        _ => print_help(),
     }
-
-    if args_len == 2 && arguments[1] == "-l" {
-        handle_list_argument();
-        return;
-    }
-
-    if args_len == 3 && arguments[1] == "-s" {
-        handle_search_argument(&arguments[2]);
-        return;
-    }
-
-    print_help();
 }
 
 fn handle_list_argument() {
-    //read file and display contents
-    if let Some(contents) = read_file(&LINUX_FILE_PATH) {
+    if let Some(contents) = read_file(LINUX_FILE_PATH) {
         print_contents(&contents);
-        return;
     }
-    return;
 }
 
 fn read_file(filepath: &str) -> Option<String> {
-    let mut file;
-    match File::open(filepath) {
-        Ok(f) => {
-            file = f;
-        }
+    let mut file = match File::open(filepath) {
+        Ok(f) => f,
         Err(e) => {
             exit_with_error(&format!("Error opening file: {}", e));
             return None;
         }
-    }
+    };
 
     let mut contents = String::new();
     match file.read_to_string(&mut contents) {
         Ok(_) => Some(contents),
         Err(e) => {
             exit_with_error(&format!("Error Reading file: {}", e));
-            return None;
+            None
         }
     }
 }
 
 fn handle_search_argument(needle: &str) {
-    let haystack;
-    match read_file(&LINUX_FILE_PATH) {
-        Some(c) => {
-            haystack = c;
-        }
-        None => {
-            return;
-        }
-    }
+    if let Some(haystack) = read_file(LINUX_FILE_PATH) {
+        let found: Vec<&str> = haystack
+            .lines()
+            .filter(|line| line.to_lowercase().contains(&needle.to_lowercase()))
+            .collect();
 
-    let found = haystack
-        .lines()
-        .filter_map(|line| {
-            if line.to_lowercase().contains(&needle.to_lowercase()) {
-                Some(line)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<&str>>();
-    for line in found {
-        print_contents(&line);
+        for line in found {
+            print_contents(line);
+        }
     }
-    return;
 }
 
 fn print_help() {
     println!("Usage [command] [arguments]");
-    return;
 }
 
 fn exit_with_error(e: &str) {
@@ -95,6 +63,5 @@ fn exit_with_error(e: &str) {
 }
 
 fn print_contents(contents: &str) {
-    println!("{contents}");
-    return;
+    println!("{}", contents);
 }
