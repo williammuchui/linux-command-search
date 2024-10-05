5f463095e80d82b9c3db3f7aa869249a845ad85a
William Muchui <williammuchui584@gmail.com>
2024-10-05 11:32:06 +0300

    add listing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::env::args;
+use std::fs::File;
+use std::io::Read;
+use std::process::exit;
+const LINUX_FILE_PATH: &str = "linux";
 
 fn main() {
     let arguments = args().collect::<Vec<String>>();
@@ -24,7 +28,33 @@ fn main() {
 
 fn handle_list_argument() {
     //read file and display contents
+    if let Some(contents) = read_file(&LINUX_FILE_PATH) {
+        print_contents(&contents);
+        return;
+    }
     return;
+}
+
+fn read_file(filepath: &str) -> Option<String> {
+    let mut file;
+    match File::open(filepath) {
+        Ok(f) => {
+            file = f;
+        }
+        Err(e) => {
+            exit_with_error(&format!("Error opening file: {}", e));
+            return None;
+        }
+    }
+
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => Some(contents),
+        Err(e) => {
+            exit_with_error(&format!("Error Reading file: {}", e));
+            return None;
+        }
+    }
 }
 
 fn handle_search_argument(needle: &str) {
@@ -33,5 +63,16 @@ fn handle_search_argument(needle: &str) {
 }
 
 fn print_help() {
+    println!("Usage [command] [arguments]");
+    return;
+}
+
+fn exit_with_error(e: &str) {
+    eprintln!("ERROR: \n{}", e);
+    exit(1);
+}
+
+fn print_contents(contents: &str) {
+    println!("{contents}");
     return;
 }
