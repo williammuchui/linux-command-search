75f9b62e707a4eca79ec39cd35addd2f5463d608
William Muchui <williammuchui584@gmail.com>
2024-10-09 14:14:49 +0300

    add exact command pattern matching

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     match arguments.as_slice() {
         [_] => handle_list_argument(),
         [_, arg] if arg == "-l" || arg == "--list" => handle_list_argument(),
-        [_, arg, needle] if arg == "-s" || arg == "--search" => handle_search_argument(needle),
+        [_, arg, needle] if arg == "-s" || arg == "--search" => handle_search_exact_command(needle),
         [_, arg] if arg == "-h" => print_help(),
         _ => print_help(),
     }
@@ -41,18 +41,30 @@ fn read_file(filepath: &str) -> Option<String> {
     }
 }
 
-fn handle_search_argument(needle: &str) {
+fn handle_search_exact_command(needle: &str) {
     if let Some(haystack) = read_file(LINUX_FILE_PATH) {
         let found: Vec<&str> = haystack
             .lines()
-            .filter(|line| line.to_lowercase().contains(&needle.to_lowercase()))
+            .filter(|line| {
+                let bytes = line.as_bytes();
+                let mut first_word = "";
+                for (i, &byte) in bytes.iter().enumerate() {
+                    if byte == b']' {
+                        first_word = &line[1..i];
+                    }
+                }
+                return first_word.contains(&needle.to_lowercase());
+            })
             .collect();
 
+        if &found.len() == &0 {
+            println!("Command not Found");
+            return;
+        }
+
+        println!("MATCHING COMMANDS");
         for line in &found {
             print_contents(line);
-        }
-        if &found.len() == &0 {
-            println!("No Command found");
         }
     }
 }
